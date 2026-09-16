b565c3df22e699b53d543d2fb5a4e5efc9d0837b
jike-gml <triton.jike@gmail.com>
2026-09-16 13:40:09 +0900

    Update PWA to v0.7 with mobile calendar improvements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'foodlog-v13';
+const CACHE = 'foodlog-v8';
 const ASSETS = [
   './',
   './index.html',
@@ -14,10 +14,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key.startsWith('foodlog-') && key !== CACHE)
-        .map((key) => caches.delete(key))
-    ))
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
   );
   self.clients.claim();
 });
